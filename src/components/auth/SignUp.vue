1551dc88bb4aaef3d99700d0f11d713ce55272aa
agungd3v <agungd3v@gmail.com>
2021-10-06 22:20:03 +0700

    add register user

--- src/components/auth/SignUp.vue
+++ src/components/auth/SignUp.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full p-14">
     <h3 class="text-center text-2xl font-bold">Sign Up</h3>
-    <form class="mt-12">
+    <form @submit.prevent="signup" class="mt-12">
       <div class="mb-5">
         <input type="text" class="custom__input_field" v-model="regName" autocomplete="off" placeholder="Full name">
       </div>
@@ -9,7 +9,15 @@
         <input type="email" class="custom__input_field" v-model="regEmail" autocomplete="off" placeholder="Email address">
       </div>
       <div class="mb-5">
-        <vue-phone-number-input v-model="regPhone" />
+        <vue-phone-number-input
+          @update="checkNumber"
+          v-model="dumpNumber"
+          :no-country-selector="true"
+          :default-country-code="defaultCountry"
+          :valid-color="validColor"
+          :color="color"
+          :show-code-on-list="true"
+        />
       </div>
       <div class="mb-5">
         <input type="password" class="custom__input_field" v-model="regPassword" placeholder="Password">
@@ -17,7 +25,7 @@
       <div class="mb-5 flex gap-1">
         <div>
           <label class="check__container">
-            <input type="checkbox">
+            <input type="checkbox" v-model="regCheck">
             <span class="check__mark"></span>
           </label>
         </div>
@@ -46,7 +54,41 @@ export default {
       regName: null,
       regEmail: null,
       regPhone: null,
-      regPassword: null
+      regPassword: null,
+      regCheck: false,
+
+      dumpNumber: null,
+      defaultCountry: 'ID',
+      color: '#1E90FF',
+      validColor: '#1E90FF'
+    }
+  },
+  methods: {
+    async signup() {
+      if (!this.regName || !this.regEmail || !this.regPassword) return false
+      if (!this.regPhone) return false
+      if (!this.regPhone.valid) return false
+      if (!this.regCheck) return false
+      this.$store.dispatch('setLoader', true)
+      try {
+        await this.$socket.emit('register', {
+          name: this.regName,
+          email: this.regEmail,
+          phone: this.regPhone.number,
+          password: this.regPassword
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    checkNumber(payload) {
+      if (payload.countryCode == 'ID') {
+        this.color = payload.isValid ? '#1E90FF' : '#EF4444'
+        this.regPhone = {
+          valid: payload.isValid,
+          number: payload.formattedNumber
+        }
+      }
     }
   }
 }
--- src/store/webSocketPlugin.js
+++ src/store/webSocketPlugin.js
@@ -3,5 +3,14 @@ export default function webSocketPlugin (socket) {
     store.$socket = socket
     socket.on('connect_error', () => store.dispatch('setLoader', true))
     socket.on('connect', () => store.dispatch('setLoader', false))
+
+    socket.on("logreg", payload => {
+      if (payload.status) {
+        store.dispatch('setLoader', false)
+        console.log(payload)
+      } else {
+        console.error(payload)
+      }
+    })
   }
 }
